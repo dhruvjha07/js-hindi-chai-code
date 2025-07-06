6127c989fdc4e22c1eb82fac2b82744ed48df050
dhruv jha <132841662+dhruvjha07@users.noreply.github.com>
2025-07-06 22:06:42 +0000

    conversion of datatype

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,19 @@
+let score = "22abc"
+
+console.log(typeof score);
+
+
+let valueInNumber = Number (score)
+console.log(valueInNumber);
+
+
+
+// "33"= 33
+//  "33abc" = NaN
+//  true = 1; false = 0;
+
+
+let isLoggedIn = "dhruv"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
