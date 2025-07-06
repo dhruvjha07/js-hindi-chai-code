aa0fd337f33ec85c1cbe51bbe8afe0b28980dcaf
dhruv jha <132841662+dhruvjha07@users.noreply.github.com>
2025-07-06 21:50:56 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 953498
+let accountEmail = "dhruvjha9534@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+// const accountId: any
+
+
+// accountId = 2 
+accountEmail = "rtrdhruv@gmail.com"
+accountPassword = "953498"
+accountCity = "bihar"
+let accountstate;
+// console.log([accountEmail , accountPassword , accountCity]); 
+
+console.table([accountEmail , accountPassword , accountCity , accountstate]);
+//  prefer not to use var 
+// 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(8 + 1 ) we are using node.js not browser 
+
+console.log(3+3); 
+console.log("dhruv")
+
+
+
+let name = "dhruv"
+let age = 20
+let isLoggedIn = true
+
+
+// number => 2 to power 53
+// bigint
+// string  prefered in double qoute 
+// boolean => true/false 
+// null => standalone value 
+// undefined => value not define 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof  "dhruv");
