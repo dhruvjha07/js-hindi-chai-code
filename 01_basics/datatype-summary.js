2193e2362a543e99324f8c0e85a78336fad82f95
dhruv jha <132841662+dhruvjha07@users.noreply.github.com>
2025-07-08 18:04:32 +0000

    datatype summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,35 @@
+// primitive data types
+
+//  7 categories 
+// String , Number , Boolean , null , undefined , symbol , BigInt
+
+// const score = 200
+// const score = 200.09
+
+const isLoggedIN = false
+const outsideTemp = null
+let useremail;
+
+const id = Symbol('1234')
+const anotherid = Symbol('122')
+
+console.log(id === anotherid);
+
+
+
+// reference type or non primitve 
+
+// Array , Objects , functions
+
+
+
+const heros = ["dj" , "srk" ,"bigb" ]
+
+let myobj = {
+    name: " dhruv"
+    age: "21"
+    class: "IT"
+
+}
+
+const 
