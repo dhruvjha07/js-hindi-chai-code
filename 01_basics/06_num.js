d8db49dde06b1d48626e9722b66ebacbfb0872b8
dhruv jha <132841662+dhruvjha07@users.noreply.github.com>
2025-07-10 12:55:12 +0000

    num&maths

---
+++ 01_basics/06_num.js
@@ -0,0 +1,36 @@
+const score = 400 
+
+const balance = new Number(100.80)
+// console.log(balance);
+
+
+// console.log(balance.toFixed(3));
+
+const otherNumber = 7253.4322
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+///////////////////////// MATHS /////////////////////////////
+
+
+console.log(Math);
+console.log(Math.abs(-34));
+console.log(Math.round(4.66));
+console.log(Math.floor(4.9));
+console.log(Math.sqrt(25));
+console.log(Math.max(4, 3, 5, 6));
+
+
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min);
